abf915c313b93b337e0e8cc2b3f5304103a7e608
Sota Nakai <soooota1201@gmail.com>
2021-03-04 20:30:58 +0900

    first commit

---
+++ cli.php
@@ -0,0 +1,60 @@
+<?php 
+
+$app = new App();
+$app->execute();
+
+class App 
+{
+  protected function message($message)
+  {
+    echo escapeshellcmd($message) . "\n";
+  }
+
+  protected function ask($message)
+  {
+    $this->message($message);
+    return trim(fgets(STDIN));
+    //標準入力
+  }
+
+  protected function listen()
+  {
+    $age = $this->ask('年齢を入力してください');
+
+    if(!is_numeric($age)) {
+      return false;
+    } else {
+      return $age;
+    }
+  }
+
+  protected function isAdult($age) {
+    return (20 <= $age);
+  }
+
+  protected function isOld($age)
+  {
+    return (80 <= $age);
+  }
+
+  public function execute()
+  {
+    $age = $this->listen();
+    if(false === $age) {
+      $this->message('年齢を入力して！！');
+      return;
+    }
+    if ($this->isOld($age)) {
+      $this->message('ご高齢です。');
+    } elseif ($this->isAdult($age)) {
+      $this->message('あなたは大人です。');
+    } else {
+      $this->message('お子ちゃまですね。早く寝なさい！！');
+    }
+
+    // if($this->isOld($age))
+    // {
+    //   $this->message('ご高齢です。');
+    // }
+  }
+}
